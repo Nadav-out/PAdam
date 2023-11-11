a645e505a8357f36aec6f5c41b71204412880c72
Nadav-Out <nadav.out@gmail.com>
2023-11-10 23:14:59 -0800

    Import files

---
+++ python/models.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FashionCNN(nn.Module):
+    def __init__(self):
+        super(FashionCNN, self).__init__()
+        
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+                
+        # Fully connected layers
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+        # Dropout
+        self.dropout = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, stride=2)
+        
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, stride=2)
+        
+        # reshape
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        
+        # Dropout
+        x = self.dropout(x)
+        x = self.fc2(x)
+        
+        return F.log_softmax(x, dim=1)
+
+
