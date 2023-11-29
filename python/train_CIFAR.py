ed2b5363fe8d1450f0deaa6a52a6b8809d1daa34
Nadav J Outmezguine <Nadav.Out@gmail.com>
2023-11-29 00:23:43 -0800

    script

--- python/train_CIFAR.py
+++ python/train_CIFAR.py
@@ -10,6 +10,7 @@ from torch.utils import data as dataloader
 import copy
 import time
 import numpy as np
+import math
 
 from models import *
 from Optimizers import *
@@ -37,23 +38,25 @@ epochs = 100
 
 # optimizer
 optimizer_name = 'AdamW'
-max_lr = 3e-3
-lambda_p = 1e-1
+max_lr = 1e-3
+lambda_p = 1e-3
 p_norm = 0.8
 beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
-
-warmup_frac = 0.05 # fraction of warm up steps
-lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
+warmup_iters = 1000 # how many steps to warm up for
+lr_decay_epochs = int(0.8*epochs) # should be ~= max_iters per Chinchilla
 min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
+
+small_weights_threshold = 1e-13 # weights smaller than this will be considered "small"
+
 
 # DDP settings
 backend = 'nccl' # 'nccl', 'gloo', etc.
 # system
-device = 'cpu' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
+device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
 compile = True # use PyTorch 2.0 to compile the model to be faster
 # -----------------------------------------------------------------------------
@@ -137,4 +140,126 @@ if compile:
     unoptimized_model = model
     model = torch.compile(model) # requires PyTorch 2.0
 
+criterion = torch.nn.CrossEntropyLoss()
 
+
+lr_decay_iters=len(trainloader)*lr_decay_epochs
+
+# learning rate decay scheduler (cosine with warmup)
+def get_lr(it):
+    # 1) linear warmup for warmup_iters steps
+    if it < warmup_iters:
+        return max_lr * it / warmup_iters
+    # 2) if it > lr_decay_iters, return min learning rate
+    if it > lr_decay_iters:
+        return min_lr
+    # 3) in between, use cosine decay down to min learning rate
+    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff ranges 0..1
+    return min_lr + coeff * (max_lr - min_lr)
+
+
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+model_save_path = os.path.join(out_dir, 'best_model.pth')
+stats_save_path = os.path.join(out_dir, 'training_stats.pkl')
+
+
+
+best_accuracy = 0.0
+iteration_count = 0
+train_losses = []
+val_losses = []
+accuracies = []
+lrs = []
+small_weight_fractions = []
+
+
+start_time = time.time()    
+for epoch in range(epochs):
+    model.train()
+    running_loss = 0.0
+    for data in trainloader:
+            # determine and set the learning rate for this iteration
+        lr = get_lr(iteration_count) if decay_lr else max_lr
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+        iteration_count += 1
+        inputs, labels = data
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+        optimizer.step()
+
+        running_loss += loss.item()
+        # scheduler.step()
+
+    avg_train_loss = running_loss / len(trainloader)
+    train_losses.append(avg_train_loss)
+
+    # Validation loop
+    model.eval()
+    correct = 0
+    total = 0
+    running_val_loss = 0.0
+
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            running_val_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            
+
+
+
+    avg_val_loss = running_val_loss / len(testloader)
+    val_losses.append(avg_val_loss)
+
+    accuracy = 100 * correct / total
+    accuracies.append(accuracy)
+
+
+    small_weight_fractions.append([fraction_small_weights(param, small_weights_threshold) for param in model.parameters() if param.requires_grad])
+    
+    # Save best model
+    if accuracy > best_accuracy:
+        best_accuracy = accuracy
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'accuracy': accuracy,
+        }, model_save_path)
+
+    # Calculate and format runtime and expected time
+    elapsed_time = time.time() - start_time
+    expected_time = elapsed_time * epochs / (epoch + 1)
+    elapsed_str = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
+    expected_str = time.strftime("%H:%M:%S", time.gmtime(expected_time))
+
+    # Track and store current learning rate
+    current_lr = optimizer.param_groups[0]['lr']
+    lrs.append(current_lr)
+
+    status_message = f"Epoch: {epoch+1}/{epochs}\tTrain Loss: {avg_train_loss:.4f}\tTest Loss: {avg_val_loss:.4f}\tAccuracy: {accuracy:.2f}%\tCurrent LR: {current_lr:.5f}\tElapsed Time: {elapsed_str}\tExpected Time: {expected_str}"
+    print(f"\r{status_message}",end='')
+
+print()
+
+
+with open(stats_save_path, 'wb') as f:
+    pickle.dump({
+        'train_losses': train_losses,
+        'val_losses': val_losses,
+        'accuracies': accuracies,
+        'lrs': lrs
+    }, f)
