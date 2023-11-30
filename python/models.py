17305a4e6a2c05bf9882c59dca2c980c9fe5b43f
Nadav J Outmezguine <Nadav.Out@gmail.com>
2023-11-29 16:51:42 -0800

    orginizing

--- python/models.py
+++ python/models.py
@@ -49,47 +49,10 @@ class FashionCNN(nn.Module):
         
         return F.log_softmax(x, dim=1)
 
-
-
-
-
-
-    
-def accuracy(outputs, labels):
-    _, preds = torch.max(outputs, dim=1)
-    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
-
-class ImageClassificationBase(nn.Module):
-    def training_step(self, batch):
-        images, labels = batch 
-        out = self(images)                  # Generate predictions
-        loss = F.cross_entropy(out, labels) # Calculate loss
-        return loss
-    
-    def validation_step(self, batch):
-        images, labels = batch 
-        out = self(images)                    # Generate predictions
-        loss = F.cross_entropy(out, labels)   # Calculate loss
-        acc = accuracy(out, labels)           # Calculate accuracy
-        return {'val_loss': loss.detach(), 'val_acc': acc}
-        
-    def validation_epoch_end(self, outputs):
-        batch_losses = [x['val_loss'] for x in outputs]
-        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
-        batch_accs = [x['val_acc'] for x in outputs]
-        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
-        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
-    
-    def epoch_end(self, epoch, result):
-        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
-            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))
-        
-
-        
         
         
 def conv_block(in_channels, out_channels, activation=False, pool=False):
-    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1,bias=False), 
               nn.BatchNorm2d(out_channels)]
     if activation: layers.append(nn.ReLU(inplace=True))
     if pool: layers.append(nn.MaxPool2d(2))
@@ -124,7 +87,12 @@ class ResNet18(nn.Module):
                                         nn.Dropout(0.1),
                                         nn.Linear(512, num_classes))
         
+        
+        # Store the number of parameters for each layer
         self.apply(self.init_weights)
+        # Initialize and count parameters
+        self.layer_param_count, self.layer_regulated_param_count = self.count_parameters()
+
 
     def init_weights(self,m):
         if isinstance(m, nn.Conv2d):
@@ -154,12 +122,11 @@ class ResNet18(nn.Module):
         decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
         nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
         
-        print(f'WD={weight_decay}')
         num_decay_params = sum(p.numel() for p in decay_params)
         num_nodecay_params = sum(p.numel() for p in nodecay_params)
         print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
         print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
-        # Create AdamW optimizer and use the fused version if it is available
+        # Create AdamW/PAdam optimizer and use the fused version if it is available
         fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
         use_fused = fused_available and device_type == 'cuda'
         extra_args = dict(fused=True) if use_fused else dict()
@@ -176,7 +143,45 @@ class ResNet18(nn.Module):
                 ]
             optimizer = PAdam(optim_groups, lr=learning_rate,p_norm=p_norm, betas=betas, **extra_args)
         
-        print(f"using fused AdamW: {use_fused}")
+        print(f"using fused Adam: {use_fused}")
 
         return optimizer
+    
+    def count_parameters(self):
+        layer_param_count = {}
+        layer_regulated_param_count = {}  # For regulated (weight decay) parameters
+
+        for name, param in self.named_parameters():
+            if param.requires_grad:
+                # Extract module name from parameter name
+                module_name = name.split('.')[0]
+                
+                # Count all trainable parameters
+                layer_param_count[module_name] = layer_param_count.get(module_name, 0) + param.numel()
+
+                # Count regulated parameters (2D or higher)
+                if param.dim() >= 2:
+                    layer_regulated_param_count[module_name] = layer_regulated_param_count.get(module_name, 0) + param.numel()
+
+        total_params = sum(layer_param_count.values())
+        print(f"Number of trainable parameters: {total_params:,}")
+
+        return layer_param_count, layer_regulated_param_count
+
+    
+    def count_small_regulated_weights(self, threshold=1e-13):
+        small_weights_count = {}
+        for name, param in self.named_parameters():
+            if param.requires_grad and param.dim() >= 2:  # Regularized weights
+                # Extract the higher-level module name from the parameter name
+                module_name = name.split('.')[0]
+                # Count the small weights in this parameter
+                count = (param.abs() < threshold).sum().item()
+                if module_name in small_weights_count:
+                    small_weights_count[module_name] += count
+                else:
+                    small_weights_count[module_name] = count
+        return small_weights_count
+
+
 
