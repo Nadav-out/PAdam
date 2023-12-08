a8794fbedd28cab31dacd9b14b3f2c8a04800f78
Nadav J Outmezguine <Nadav.Out@gmail.com>
2023-12-08 15:34:25 -0800

    fashion maunal p-norm and AdamP

--- python/Optimizers.py
+++ python/Optimizers.py
@@ -30,3 +30,36 @@ class PAdam(torch.optim.Adam):
                 param.data.mul_(update_term)
 
         return loss
+
+
+
+class AdamP(torch.optim.Adam):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
+                 lambda_p=0, amsgrad=False, p_norm=2):
+        # Initialize the parent class (Adam) with weight_decay set to 0
+        super(AdamLp, self).__init__(params, lr=lr, betas=betas, eps=eps,
+                                     weight_decay=0, amsgrad=amsgrad)
+        self.lambda_p = lambda_p
+        self.p_norm = p_norm
+
+    def step(self, closure=None):
+        # Compute the loss using the parent class's step method if a closure is provided
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        # Apply custom Lp^p regularization to the gradients
+        for group in self.param_groups:
+            for param in group['params']:
+                if param.grad is None:
+                    continue
+
+                # Apply the general Lp^p regularization
+                if self.lambda_p != 0:
+                    lp_grad = (param.data.abs()**(self.p_norm - 2)) * param.data
+                    param.grad.data.add_(lp_grad, alpha=self.p_norm * self.lambda_p)
+
+        # Perform the optimization step using the parent class's logic
+        super(AdamP, self).step(closure)
+
+        return loss
