331c938d86eed92867ac5ef570601befd7163602
Nadav J Outmezguine <Nadav.Out@gmail.com>
2023-12-08 22:35:25 -0800

    adam tests

--- python/Optimizers.py
+++ python/Optimizers.py
@@ -63,3 +63,49 @@ class AdamP(torch.optim.Adam):
         super(AdamP, self).step(closure)
 
         return loss
+
+
+class CustomAdam(torch.optim.Optimizer):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        super(CustomAdam, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+
+                grad = p.grad.data
+                if group['weight_decay'] != 0:
+                    grad.add_(p.data, alpha=group['weight_decay'])
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                state['step'] += 1
+
+                # Decay the first and second moment running average coefficient
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+
+                bias_correction1 = 1 - beta1 ** state['step']
+                bias_correction2 = 1 - beta2 ** state['step']
+                step_size = group['lr'] / bias_correction1
+
+                denom = (exp_avg_sq.sqrt() / bias_correction2.sqrt()).add_(group['eps'])
+                p.data.addcdiv_(-step_size, exp_avg, denom)
+
+        return loss
